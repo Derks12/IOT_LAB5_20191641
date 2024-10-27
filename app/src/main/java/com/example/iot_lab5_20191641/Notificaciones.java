6246111251955888591c47defd980d0f4a735be3
Derks12 <148168249+Derks12@users.noreply.github.com>
2024-10-26 21:22:09 -0500

    faltan dos notis

--- app/src/main/java/com/example/iot_lab5_20191641/Notificaciones.java
+++ app/src/main/java/com/example/iot_lab5_20191641/Notificaciones.java
@@ -1,18 +1,37 @@
 package com.example.iot_lab5_20191641;
 
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.iot_lab5_20191641.databinding.ActivityNotificacionesBinding;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Notificaciones extends AppCompatActivity {
 
+    String channelId = "channelDefaultPri";
     private ImageView back2;
+    private TextInputEditText tiempoEditText;
 
 
     @Override
@@ -27,6 +46,7 @@ public class Notificaciones extends AppCompatActivity {
         });
 
 
+        createNotificationChannel();
 
         back2 = findViewById(R.id.back2);
         back2.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +56,77 @@ public class Notificaciones extends AppCompatActivity {
             }
         });
 
+        tiempoEditText = findViewById(R.id.textFieldPeso).findViewById(com.google.android.material.R.id.textinput_placeholder);;
+
+        // Botón para configurar la notificación
+        Button buttonRegistrarComida = findViewById(R.id.buttonRegistrarComida);
+        buttonRegistrarComida.setOnClickListener(v -> {
+            String minutosStr = tiempoEditText.getText().toString();
+            if (!minutosStr.isEmpty()) {
+                int minutos = Integer.parseInt(minutosStr);
+                programarNoti(minutos);
+            }
+        });
+
 
     }
+
+    public void createNotificationChannel() {
+        //android.os.Build.VERSION_CODES.O == 26
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Canal notificaciones default",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Canal para notificaciones con prioridad default");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+            askPermission();
+        }
+    }
+
+    public void askPermission(){
+        //android.os.Build.VERSION_CODES.TIRAMISU == 33
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                        PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(Notificaciones.this,
+                    new String[]{POST_NOTIFICATIONS},
+                    101);
+        }
+    }
+
+    //le pregunté a chatgpt para usar alarmManager
+    private void programarNoti(int intervalMinutes) {
+
+        Intent intent = new Intent(this, Notificaciones.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        long intervalMillis = intervalMinutes * 60 * 1000L;
+        long triggerTime = System.currentTimeMillis() + intervalMillis;
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerTime, intervalMillis, pendingIntent);
+        showNotification();
+    }
+
+    private void showNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_logo) // Icono de la notificación
+                .setContentTitle("Mensaje de motivación")
+                .setContentText("Tú puedes, sigue tu objetivo")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+        }
+    }
+
 }
